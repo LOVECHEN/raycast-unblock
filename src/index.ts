b948213146074081c2863ad50d7af41d20ffa4c6
wibus-wee <1596355173@qq.com>
2024-02-02 22:46:53 +0800

    feat: init fastify server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
-export const one = 1
-export const two = 2
+import process from 'node:process'
+import Fastify from 'fastify'
+import packageJson from '../package.json'
+
+const fastify = Fastify({ logger: true })
+
+fastify.get('/', async (_request, _reply) => {
+  return {
+    name: packageJson.name,
+    version: packageJson.version,
+    author: packageJson.author,
+  }
+})
+
+fastify.listen({ port: 3000 }, (err, address) => {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  fastify.log.info(`Server listening on ${address}`)
+})
