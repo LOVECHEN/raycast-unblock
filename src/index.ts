eefd1a8cabbc1aba6e10f742c7aef0d20b64f726
wibus-wee <1596355173@qq.com>
2024-02-03 14:25:45 +0800

    feat: check ai config

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import process from 'node:process'
 import packageJson from '../package.json'
 import { prepareSync } from './features/sync/pre'
 import { launch } from './launch'
+import { checkAIConfig } from './utils/env.util'
 
 process.title = packageJson.name
 
 launch()
 prepareSync()
+checkAIConfig()
--- src/utils/env.util.ts
+++ src/utils/env.util.ts
@@ -1,4 +1,5 @@
 import process from 'node:process'
+import consola from 'consola'
 import type { AIConfig } from '../types'
 
 export function getAIConfig(): AIConfig {
@@ -9,4 +10,11 @@ export function getAIConfig(): AIConfig {
     max_tokens: process.env.AI_MAX_TOKENS,
     temperature: process.env.AI_TEMPERATURE || '0.5',
   }
+}
+
+export function checkAIConfig() {
+  const config = getAIConfig()
+  consola.info('Your AI will be using [', config.type, '] API')
+  if (!config.key)
+    consola.warn('AI_API_KEY is not set')
 }
