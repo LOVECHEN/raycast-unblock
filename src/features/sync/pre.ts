43072d4c6c674fd9bedcc919e193588c6937a13d
wibus-wee <1596355173@qq.com>
2024-02-03 09:37:13 +0800

    feat: support different platform

--- src/features/sync/pre.ts
+++ src/features/sync/pre.ts
@@ -5,7 +5,11 @@ import consola from 'consola'
 
 export function prepareSync() {
   consola.info('[Sync] Checking sync folder availability...')
-  const syncPath = resolve(os.homedir(), 'Library/Mobile Documents/com~apple~CloudDocs/RaycastSync')
+  let syncPath = resolve(os.homedir(), '.raycast_sync')
+  if (os.platform() === 'darwin') {
+    consola.info('[Sync] macOS detected, using iCloud Drive as sync folder...')
+    syncPath = resolve(os.homedir(), 'Library/Mobile Documents/com~apple~CloudDocs/RaycastSync')
+  }
   if (!fs.existsSync(syncPath)) {
     consola.info('[Sync] Preparing sync folder...')
     fs.mkdirSync(syncPath)
