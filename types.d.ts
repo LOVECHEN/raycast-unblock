c6b850800fcef8706114e0d5b3216e8f718f0d1e
wibus-wee <1596355173@qq.com>
2024-02-05 09:33:14 +0800

    feat: support custom launch port

--- types.d.ts
+++ types.d.ts
@@ -3,12 +3,14 @@ declare global {
     interface ProcessEnv {
       users: string
       NODE_ENV: 'development' | 'production'
-      AI_TYPE: 'openai' | 'gemini' | 'custom'
+      AI_TYPE: 'openai' | 'gemini' | 'custom' | 'copilot'
       AI_API_KEY: string
       AI_ENDPOINT: string
       AI_MAX_TOKENS: string
       AI_TEMPERATURE: string
-      TRANSLATE_TYPE: 'shortcut' | 'ai'
+      TRANSLATE_TYPE: 'shortcut' | 'ai' | 'custom' | 'github'
+      TRANSLATE_API_KEY?: string
+      PORT: string
     }
   }
 }
