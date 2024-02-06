90742afec1b4463238492125743ce8d2db4c3fde
wibus-wee <1596355173@qq.com>
2024-02-06 14:25:51 +0800

    fix(external): rewrite node-fetch to disable deprecated module warning

---
+++ external/node-fetch/index.js
@@ -0,0 +1,7 @@
+// Why rewrite node-fetch?
+// The upstream library whatwg-url uses the punycode module, but the punycode module will be deprecated in node21
+// As a result, we need to rewrite node-fetch to remove the dependency on the punycode module
+// The rewrite is only for the export of fetch.
+
+export default fetch
+module.exports = fetch
